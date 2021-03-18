ca76be2552448d5bb1e27271a9e047630f514361
Jérôme Pasquier <jerome.pasquier@unisante.ch>
2021-03-18 14:04:46 +0100

    prev_serocovid_compl.R 2021-02-18 2021-03-02

--- prev_serocovid_compl.R
+++ prev_serocovid_compl.R
@@ -7,14 +7,19 @@ setwd("~/Projects/SerocoViD")
 load("results/prev_serocovid_20210218.dta")
 
 #
-dta3$all <- "all"
-dta3$agegroup3 <- factor(dta3$stratum, 4:8,
-                         c("15-19", "20-64", "20-64", ">=65", ">=65"))
-ppos <- Mean(data = dta3, variable = "serol_any", stratum = "stratum",
-        domain = c("all", "agegroup3"), pop = pop1)
-ppos$v <- NULL
-names(ppos)[names(ppos) == "y"] <- "ppos"
-ppos <- cbind(antibody = "IgG or IgA", ppos)
+ppos <- do.call(rbind, lapply(1:3, function(k) {
+  dta <- get(paste0("dta", k))
+  dta <- subset(dta, stratum >= 4)
+  dta$all <- "all"
+  dta$agegroup3 <- factor(dta$stratum, 4:8,
+                          c("15-19", "20-64", "20-64", ">=65", ">=65"))
+  pop <- list(pop1, pop1, pop3)[[k]]
+  ppos <- Mean(data = dta, variable = "serol_any", stratum = "stratum",
+          domain = c("all", "agegroup3"), pop = pop)
+  ppos$v <- NULL
+  names(ppos)[names(ppos) == "y"] <- "ppos"
+  ppos <- cbind(phase = k, antibody = "IgG or IgA", ppos)
+}))
 
 #
-write_xlsx(ppos, "results/prev_serocovid_20210218_compl_20210301.xlsx")
+write_xlsx(ppos, "results/prev_serocovid_20210218_compl_20210302.xlsx")
