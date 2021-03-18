9555f97c7b71d64c207141c53ec7d442e4a91a15
Jérôme Pasquier <jerome.pasquier@unisante.ch>
2021-03-18 11:24:59 +0100

    component1.R 2020-07-13

--- component1.R
+++ component1.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(lme4)
+library(gee)
 
 # Working directory
 setwd("~/Projects/SerocoViD")
@@ -18,9 +19,12 @@ sero$age_cat_2 <- factor(sero$age_cat_2)
 sero$index_case_age_cat_2 <- factor(sero$index_case_age_cat_2)
 
 # Fixed effects models
+m0 <- glm(sero_result ~ 1, family = binomial, data = sero)
 m1 <- glm(sero_result ~ age_cat_2, family = binomial, data = sero)
 summary(m1)
-exp(coef(m1))  # odds ratios
+unique(predict(m1, type = "response"))
+aggregate(sero_result ~ age_cat_2, sero, mean)
+exp(cbind(coef(m1), confint(m1)))  # odds ratios
 m2 <- glm(sero_result ~ index_case_age_cat_2, family = binomial, data = sero)
 summary(m2)
 exp(coef(m2))  # odds ratios
@@ -30,10 +34,14 @@ summary(m3)
 exp(coef(m3))  # odds ratios
 
 # Mixed effect models
+m4 <- glmer(sero_result ~ 1 + (1 | index_case_hid),
+            family = binomial, data = sero)
+predict(m4, re.form = ~0, type = "response")
 m4 <- glmer(sero_result ~ age_cat_2 + (1 | index_case_hid),
             family = binomial, data = sero)
+unique(predict(m4, re.form = ~0, type = "response"))
 summary(m4)
-exp(fixef(m4))  # odds ratios
+exp(cbind(fixef(m4), confint(m4, parm = "beta_")))  # odds ratios
 m5 <- glmer(sero_result ~ index_case_age_cat_2 + (1 | index_case_hid),
             family = binomial, data = sero)
 summary(m5)
@@ -47,5 +55,10 @@ exp(fixef(m6))  # odds ratios
 # sero by cluster
 u <- aggregate(sero_result ~ index_case_hid, sero, mean)[, 2]
 table(ifelse(u == 0, "[0]", ifelse(u == 1, "[1]", "(0,1)")))
+a <- merge(sero[c("index_case_hid", "age_cat_2")],
+      aggregate(sero_result ~ index_case_hid + age_cat_2, sero, median),
+      by = c("index_case_hid", "age_cat_2")
+aggregate(sero_result ~ age_cat_2, aggregate(sero_result ~ index_case_hid + age_cat_2, sero, median), median)
 
-
+ m9 <- gee(sero_result ~ age_cat_2, id=index_case_hid, family=binomial,
+             data=sero, corstr = "exchangeable")
