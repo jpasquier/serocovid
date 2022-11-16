d93157d7017c07f435e6b4a1100ca7cfc2e701ba
Jérôme Pasquier <jerome.pasquier@unisante.ch>
2022-11-16 18:58:15 +0100

    Update fig_evol_antibody_after_positive_pcr.R

--- R/fig_evol_antibody_after_positive_pcr.R
+++ R/fig_evol_antibody_after_positive_pcr.R
@@ -1,9 +1,8 @@
-library(REDCapR)
-library(survival)
+library(RCurl)
+library(dplyr)
 library(ggplot2)
-library(survminer)
-#library(ggforce)
-#library(wesanderson)
+library(ggh4x)
+library(parallel)
 
 # Working directory
 setwd("~/Projects/SerocoViD")
@@ -14,257 +13,199 @@ setwd("~/Projects/SerocoViD")
 uri   <- "https://redcap.unisante.ch/api/"
 
 # Tokens
-tokens <- c("corona_immunitas", "personal_data", "research_data")
+tokens <- c("corona_immunitas", "personal_data", "research_data",
+            "corona_immunitas_vague_6")
 tokens <- lapply(setNames(tokens, tokens), function(z) {
   z <- paste0("misc/redcap_", z, ".token")
   readChar(z, file.info(z)$size - 1)
 })
 
 # Import
-tmp_file <- "/tmp/serocovid_data.rda"
+tmp_file <- "/mnt/ramdisk/serocovid_data.rda"
 if (file.exists(tmp_file)) {
   load(tmp_file)
 } else {
-  serocovid_data <- lapply(tokens, function(token) {
-    redcap_read(redcap_uri = uri, token = token)$data
+  serocovid_data <- mclapply(tokens, function(token) {
+    read.csv(text = postForm(
+      uri = uri,
+      token = token,
+      content = 'record',
+      format = 'csv'
+    ))
   })
   save(serocovid_data, file = tmp_file)
 }
 rm(uri, tokens, tmp_file)
 
-# ------------------------ Research data - Survey 3 ------------------------- #
+# -------------------------- Prepare research data -------------------------- #
 
-# Personal data - type of participant
-vars <- c("uc_s_participant_hid", "uc_s_type_participant")
-pdta <- unique(serocovid_data$personal_data[vars])
-if (any(duplicated(pdta$uc_s_participant_hid))) {
-  stop("Duplicated hid")
-}
+dta <- bind_rows(
+  select(
+    serocovid_data$corona_immunitas, hid, uc_labo_qc_v4,
+    matches('^uc_labo_covigg(l|r)(_zscore)?_v(2|4)'), uc_ser_date_hr_kittrans,
+    matches('^pcr_(prior|date|result)(_multiple)?$'),
+    matches('^bl_vac_(yn|first|second|third)(_ph4)?$'),
+  ),
+  select(
+    serocovid_data$corona_immunitas_vague_6, hid, 
+    matches('^(spike|nuc)_igg_(qual|ratio)$'), nuc_igg_qual,
+    uc_ser_date_hr_ps_v6, bl_test_pos_yn, starts_with('bl_infection_date'),
+    matches('^bl_vac_(yn|first|second|third|fourth)(_date)?$')
+  )
+) %>%
+  left_join(
+    serocovid_data$personal_data %>%
+      unique() %>%
+      select(hid = uc_s_participant_hid, type = uc_s_type_participant,
+        matches('^uc_status_v4_(check_inf___(1|2)|time_prel_(centre|home))$')),
+    by = "hid"
+  ) %>%
+  filter(
+    type == 13 & !grepl("^123123", hid) |
+    type == 14 & !(hid %in% c("13131313", "987654321")) |
+    type == 15 & !(hid %in%
+      c("6-841282", "6-76BKAW", "6-BHSMPF", paste0("test", 1:6)))
+  ) %>%
+  mutate(
+    serol = case_when(
+      type == 13 ~ case_when(
+        grepl("(Négatif|Ind(e|é)terminé)", uc_labo_coviggl_v2) ~ 0L,
+        grepl("Positif", uc_labo_coviggl_v2) ~ 1L,
+        TRUE ~ NA_integer_
+      ),
+      type == 14 ~ case_when(
+                     uc_labo_qc_v4 != "OK" ~ NA_integer_,
+                     uc_labo_coviggl_v4 == "négatif" ~ 0L,
+                     uc_labo_coviggl_v4 == "positif" ~ 1L,
+                   ),
+      type == 15 ~ as.integer(spike_igg_qual == 1 | nuc_igg_qual == 1)
+    ),
+    serol_score = case_when(
+      type == 13 ~ uc_labo_coviggr_v2,
+      type == 14 ~ uc_labo_coviggr_zscore_v4,
+      type == 15 ~ pmax(spike_igg_ratio, nuc_igg_ratio)
+    ),
+    serol_date = as.Date(case_when(
+      type == 13 ~ uc_ser_date_hr_kittrans,
+      type == 14 ~ if_else(
+        uc_status_v4_check_inf___1 == 1 | uc_status_v4_check_inf___2 == 1,
+        uc_status_v4_time_prel_centre, uc_status_v4_time_prel_home),
+      type == 15 ~ uc_ser_date_hr_ps_v6
+    )),
+    positive_prior_test = case_when(
+      type %in% 13:14 ~ case_when(
+        pcr_prior == 0 ~ 0L,
+        pcr_prior == 1 ~ case_when(
+          as.Date(pcr_date) > serol_date ~ 0L,
+          pcr_result == 1 ~ 1L,
+          pcr_result == 2 ~ 0L,
+          TRUE ~ NA_integer_
+        ),
+        pcr_prior == 3 ~ case_when(
+          as.Date(pcr_date_multiple) > serol_date ~ 0L,
+          pcr_result_multiple == 0 ~ 0L,
+          pcr_result_multiple == 1 ~ 1L,
+          TRUE ~ NA_integer_
+        ),
+        TRUE ~ NA_integer_
+      ),
+      type == 15 ~ as.integer(bl_test_pos_yn)
+    ),
+    positive_test_date = case_when(
+      type %in% 13:14 ~ as.Date(case_when(
+        positive_prior_test == 1 ~ case_when(
+          pcr_prior == 1 ~ pcr_date,
+          pcr_prior == 3 ~ pcr_date_multiple
+        ),
+        TRUE ~ NA_character_
+      )),
+      type == 15 ~ do.call(pmax, c(across(starts_with('bl_infection_date'), 
+                                          as.Date), na.rm = TRUE))
+    ),
+    time_from_positive_test = as.integer(serol_date - positive_test_date),
+    vac = case_when(
+      type == 13 ~ recode(bl_vac_yn, `2` = 0L, `3` = NA_integer_),
+      type == 14 ~ recode(bl_vac_yn_ph4, `2` = 0L, `3` = NA_integer_),
+      type == 15 ~ recode(bl_vac_yn, `2` = NA_integer_),
+    ),
+    n_vac_before_serol = {
+      z <- .[grep('^bl_vac_(first|second|third|fourth)(_(ph4|date))?$',
+                  names(.))]
+      z <- do.call(cbind, lapply(z, function(w) as.Date(w) <= serol_date))
+      ifelse(apply(is.na(z), 1, all), NA, apply(z, 1, sum, na.rm = TRUE))
+    },
+    vac = if_else(vac %in% 1 & n_vac_before_serol %in% 0, 0L, vac),
+    vac_date = {
+      z <- .[grep('^bl_vac_(first|second|third|fourth)(_(ph4|date))?$',
+                  names(.))]
+      z <- lapply(z, function(w) {
+        w <- as.Date(w)
+        if_else(w <= serol_date, w, as.Date(NA))
+      })
+      if_else(vac == 1L, do.call(pmax, c(z, na.rm = TRUE)), as.Date(NA)) 
+    },
+    time_from_vac = as.integer(serol_date - vac_date),
+    visit = type - 10
+  )
 
-# Research data
-vars <- c("hid", "pcr_date", "pcr_result", "uc_ser_date_hr_kittrans",
-          "uc_labo_coviggl_v2", "uc_labo_covigal_v2", "uc_labo_coviggr_v2",
-          "uc_labo_covigar_v2", "bl_hosp_start", "bl_vac_yn", "bl_vac_first")
-dta3 <- serocovid_data$corona_immunitas[vars]
-dta3 <- merge(dta3, pdta, by.x = "hid", by.y = "uc_s_participant_hid",
-              all.x = TRUE)
-if (any(is.na(dta3$uc_s_type_participant))) {
-  stop("missing type of participant")
-}
-rm(vars, pdta)
+# --------------------------- Inconsistent dates ---------------------------- #
 
-# Select observation of survey 3
-dta3 <- dta3[dta3$uc_s_type_participant %in% 13, ]
+with(dta, table(visit, time_from_positive_test < 0 |
+                         time_from_positive_test > 1095))
+with(dta, table(visit, time_from_vac < 0 | time_from_vac > 1095))
+dta %>%
+  filter(visit == 4, time_from_vac < 0 | time_from_vac > 1095) %>%
+  select(hid, visit, matches('^uc_status_v4_time_prel_(centre|home)$'),
+         serol_date, matches('^bl_vac_(first|second|third)_ph4$'),
+         time_from_vac)
 
-# Remove fake participants from survey 3
-dta3 <- dta3[!grepl("^123123", dta3$hid), ]
+# --------------------------------- Figures --------------------------------- #
 
-# Serology
-if (FALSE) with(dta3, table(uc_labo_coviggl_v2, uc_labo_covigal_v2))
-for (k in 1:2) {
-  u <- c("uc_labo_coviggl_v2", "uc_labo_covigal_v2")[k]
-  v <- c("serol_igg", "serol_iga")[k]
-  dta3[[v]] <- NA
-  i <- !is.na(dta3[[u]]) & grepl("(Négatif|Ind(e|é)terminé)", dta3[[u]])
-  dta3[i, v] <- 0
-  i <- !is.na(dta3[[u]]) & grepl("Positif", dta3[[u]])
-  dta3[i, v] <- 1
-  if (all(is.na(dta3[[v]]) | dta3[[v]] %in% c("0", "1"))) {
-    dta3[[v]] <- as.numeric(dta3[[v]])
-  } else {
-    stop("error in serology recoding")
-  }
-}
-dta3$serol_any <- pmin(dta3$serol_igg + dta3$serol_iga, 1)
-rm(k, i, u, v)
+fig_vac <- lapply(3:5 , function(v) {
+  dta %>%
+    filter(visit == v, serol == 1, !is.na(time_from_vac), !is.na(serol),
+           !is.na(serol_score), time_from_vac >= 0 & time_from_vac <= 1095) %>%
+    select(serol_score, time_from_vac, n_vac_before_serol) %>%
+    mutate(doses = factor(n_vac_before_serol)) %>%
+    ggplot(aes(x = time_from_vac, y = serol_score)) +
+    geom_point(aes(colour = doses)) +
+    geom_smooth(method = lm, formula = y ~ x, se = TRUE) +
+    scale_y_continuous(trans = "log10") +
+    theme_bw() +
+    labs(x = "Time between vaccination (last dose) and serology (days)",
+         y = "IgG antibody level")
+})
 
-# Select observations for which serology is available
-with(dta3,
- all(!is.na(serol_igg) == !is.na(serol_iga)) &
- all(!is.na(serol_igg) == !is.na(uc_labo_coviggr_v2)) &
- all(!is.na(serol_igg) == !is.na(uc_labo_covigar_v2))
-)
-with(dta3, table(pcr_result %in% 1, !is.na(serol_igg)))
-dta3 <- dta3[!is.na(dta3$serol_igg), ]
-
-# Remove people with unknown vaccinated status
-# hid 3-37922994: bl_vac_yn is missing, serology is negative
-# hid 3-53497922, 3-58277322, 3-82348728 bl_vac:
-#   bl_vac_yn==3, serology is negative
-dta3[is.na(dta3$bl_vac_yn) | dta3$bl_vac_yn == 3, ]
-dta3 <- dta3[dta3$bl_vac_yn %in% 1:2, ]
-
-# Time from pcr to serology
-dta3$time_from_pcr <-
-  as.integer(as.Date(dta3$uc_ser_date_hr_kittrans) - dta3$pcr_date)
-
-# Inconsistencies in dates
-dta3[!is.na(dta3$time_from_pcr) & dta3$time_from_pcr < 0,
-      c("hid", "pcr_date", "uc_ser_date_hr_kittrans")]
-
-# Hospitalisation
-dta3$hosp <-  with(dta3, as.integer(
-  !is.na(bl_hosp_start) & !is.na(pcr_date) & 
-    abs(bl_hosp_start - pcr_date) <= 14))
-
-# Time from vaccination to serology
-dta3$time_from_vac <-
-  as.integer(as.Date(dta3$uc_ser_date_hr_kittrans) - dta3$bl_vac_first)
-dta3[dta3$bl_vac_yn == 1 & is.na(dta3$time_from_vac), ]
-
-# Inconsistencies in dates
-dta3[!is.na(dta3$time_from_vac) &
-        (dta3$time_from_vac < 0 | dta3$time_from_vac > 30),
-      c("hid", "bl_vac_first", "uc_ser_date_hr_kittrans", "time_from_vac",
-        "serol_igg")]
-
-# --------------------------------------------------------------------------- #
-
-# Figures
-options(scipen=10000)
-K <- list(with_hosp = 1, without_hosp = 2, vacc = 3, vacc_pos = 4)
-fig_log_igg <- lapply(K, function(k) {
-  dta_pcr <- subset(dta3, serol_igg == 1 & bl_vac_yn == 2 & pcr_result %in% 1)
-  dta_serol_nvac <- subset(dta3, serol_igg == 1 & bl_vac_yn == 2 & !(pcr_result %in% 1))
-  dta_serol_vac <- subset(dta3, serol_igg == 1 & bl_vac_yn == 1)
-  dta_vac <- subset(dta3, bl_vac_yn == 1)
+fig_test <- lapply(1:2, function(k) {
+  fig <- dta %>%
+    filter(visit %in% list(c(3, 5), 4)[[k]], vac == 0L, !is.na(serol),
+           !is.na(serol_score), !is.na(time_from_positive_test)) %>%
+    select(visit, serol, serol_score, time_from_positive_test) %>%
+    mutate(visit = droplevels(factor(visit, 3:5, paste('Survey', 3:5)))) %>%
+    ggplot(aes(x = time_from_positive_test, y = serol_score)) +
+    geom_point() +
+    geom_smooth(method = lm, formula = y ~ x, se = TRUE) +
+    scale_y_continuous(trans = "log10") +
+    labs(x = "Time between positive SARS-CoV-2 TDR/PCR and serology (days)",
+         y = "IgG antibody level (log scale)")
   if (k == 1) {
-    dta_pcr$hosp <- factor(dta_pcr$hosp, 0:1, c("ok", "hospitalized"))
-  } else if (k == 2) {
-    dta_pcr <- subset(dta_pcr, hosp == 0)
-    dta_serol_nvac <- subset(dta_serol_nvac, hosp == 0)
-    dta_serol_vac <- subset(dta_serol_vac, hosp == 0)
-  } else {
-    dta_pcr <- subset(dta_pcr, hosp == 0)
-    dta_serol_nvac <- subset(dta_serol_nvac, hosp == 0)
-    dta_serol_vac <- subset(dta_serol_vac, hosp == 0)
-    dta_vac <- subset(dta_vac, !is.na(time_from_vac))
-    dta_vac$status <- with(dta_vac, droplevels(
-      factor(serol_igg + (pcr_result %in% 1) * 2, 0:3,
-             c("serol-", "serol+", "neg+pcr", "TDR/PCR+"))))
-    if (k == 4) {
-      dta_vac <- subset(dta_vac, serol_igg == 1)
-      dta_vac$status <- droplevels(dta_vac$status)
-    }
+    fig <- fig +
+      facet_grid(cols = vars(visit), scales = 'free_x', space = 'free_x')
   }
-  if (k %in% 1:2) {
-    fig <- ggplot(dta_pcr, aes(x = time_from_pcr, y = uc_labo_coviggr_v2))
-  } else {
-    fig <- ggplot(dta_vac, aes(x = time_from_vac, y = uc_labo_coviggr_v2))
-  } 
-  fig <- fig +
-    geom_smooth(method = lm, formula = y ~ x, se = TRUE, color = "grey40")
-  if (k == 1) {
-    fig <- fig +
-      geom_point(aes(color = hosp)) +
-      scale_color_manual(breaks = c("hospitalized"),
-                         values = c("black", "red"))
-  } else if (k == 2) {
-    fig <- fig + geom_point()
-  } else if (k == 3) {
-    fig <- fig +
-      geom_point(aes(color = status)) +
-      scale_color_manual(values = c("blue", "black", "red"))
-  } else {
-    fig <- fig +
-      geom_point(aes(color = status)) +
-      scale_color_manual(values = c("black", "red"))
-  }
-  if (k %in% 1:2) {
-    x.breaks <- c(0, 100, 200, 300)
-    x.bp <- c(350, 400, 430)
-    w.bp <- 15
-    fig <- fig +
-      geom_boxplot(aes(x = x.bp[1], y = uc_labo_coviggr_v2), width = w.bp) +
-      geom_vline(xintercept = 375, linetype = "longdash") +
-      annotate("text", x = 250, y = 8, label = paste("N =", nrow(dta_pcr)),
-               size = 3) +
-      labs(x = "Time between positive SARS-CoV-2 TDR/PCR and serology (days)")
-  } else {
-    x.breaks <- c(0, 10, 20)
-    x.bp <- c(33, 30, 26)
-    w.bp <- 2
-    fig <- fig +
-      geom_boxplot(aes(x = x.bp[1], y = uc_labo_coviggr_v2), width = w.bp,
-                   data = dta_pcr) +
-      geom_vline(xintercept = 28, linetype = "longdash") +
-      labs(x = "Time between vaccination and serology (days)")
-  }
-  if (k == 3) {
-    fig <- fig +
-      annotate("text", x = 20, y = .1, label = paste("N =", nrow(dta_vac)),
-               size = 3)
-  } else if (k == 4) {
-    fig <- fig +
-      annotate("text", x = 20, y = 8, label = paste("N =", nrow(dta_vac)),
-               size = 3)
-  }
-  if (k == 3) {
-    fig <- fig +
-      scale_y_continuous(breaks = 10^(-1:2), trans = "log10",
-                         labels = c("0.1", "1", "10", "100"))
-  } else {
-    fig <- fig + scale_y_continuous(trans = "log10")
-  }
-  fig <- fig +
-    geom_boxplot(aes(x = x.bp[2], y = uc_labo_coviggr_v2), width = w.bp,
-                 data = dta_serol_nvac) +
-    geom_boxplot(aes(x = x.bp[3], y = uc_labo_coviggr_v2), width = w.bp,
-                 data = dta_serol_vac) +
-    scale_x_continuous(
-      breaks = c(x.breaks, x.bp),
-      labels = c(x.breaks,
-                 paste0("TDR/PCR+\nvacc-\n(", nrow(dta_pcr), ")"),
-                 paste0("NoTest+\nvacc-\n(", nrow(dta_serol_nvac), ")"),
-                 paste0("\nvacc+\n(", nrow(dta_serol_vac), ")"))
-    ) +
-    theme_bw()
-  if (k %in% c(1, 3:4)) {
-    fig <- fig +
-      theme(legend.title = element_blank(),
-            legend.justification = 0:1,
-            legend.position = c(0.02, 0.98),
-            legend.key.size = unit(0, "points"),
-            legend.text = element_text(size = rel(.6)),
-            legend.box.background = element_rect(),
-            legend.direction = "horizontal") +
-      guides(color = guide_legend(override.aes = list(size = rel(.6))))
-  }
-  fig <- fig +
-    labs(y = "IgG antibody level (log scale)")
   return(fig)
 })
-x11();fig_log_igg$with_hosp
-x11();fig_log_igg$without_hosp
-x11();fig_log_igg$vacc
-x11();fig_log_igg$vacc_pos
-jpeg("~/fig_log_igg_with_hosp.jpg", height = 3600, width = 7200, res = 1024)
-print(fig_log_igg$with_hosp)
-dev.off()
-jpeg("~/fig_log_igg_without_hosp.jpg", height = 3600, width = 7200, res = 1024)
-print(fig_log_igg$without_hosp)
-dev.off()
-jpeg("~/fig_log_igg_vacc.jpg", height = 4800, width = 7200, res = 1024)
-print(fig_log_igg$vacc)
-dev.off()
-jpeg("~/fig_log_igg_vacc_pos.jpg", height = 4800, width = 7200, res = 1024)
-print(fig_log_igg$vacc_pos)
-dev.off()
 
-# --------------------------------------------------------------------------- #
-
-# Survival analysis
-fit_igg <- survfit(Surv(time_from_pcr, 1 - serol_igg) ~ 1, data = dta3)
-surv_igg <- ggsurvplot(fit_igg, palette = "#2E9FDF",
-                       legend = "none", title = "IgG")
-
-fit_iga <- survfit(Surv(time_from_pcr, 1 - serol_iga) ~ 1, data = dta3)
-surv_iga <- ggsurvplot(fit_iga, palette = "#2E9FDF",
-                       legend = "none", title = "IgA")
-
-jpeg("~/surv_igg.jpg", height = 3600, width = 7200, res = 1024)
-print(surv_igg)
-dev.off()
-jpeg("~/surv_iga.jpg", height = 3600, width = 7200, res = 1024)
-print(surv_iga)
-dev.off()
+d <- format(Sys.Date(), "%Y%m%d")
+for (k in 1:3) {
+  jpeg(paste0("results/fig_evol_igg_after_vac_", k + 2, "_", d, ".jpg"),
+       height = 3600, width = 7200, res = 512)
+  print(fig_vac[[k]])
+  dev.off()
+}
+for (k in 1:2) {
+  jpeg(paste0("results/fig_evol_igg_after_positive_test_",
+              c("3_5", "4")[k], "_", d, ".jpg"),
+       height = 3600, width = 7200, res = 768)
+  print(fig_test[[k]])
+  dev.off()
+}
